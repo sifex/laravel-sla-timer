13f6fdaf55a6f4c64726dd2cf0ae496f941e7e0e
Alex <chess2ryme@gmail.com>
2022-07-28 12:02:58 +0100

    wip

--- tests/models/Project.php
+++ tests/models/Project.php
@@ -4,8 +4,22 @@ namespace Sifex\LaravelSlaTimer\Tests\models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\R;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Sifex\LaravelSlaTimer\Models\SlaBreachScheme;
+use Sifex\LaravelSlaTimer\Models\SlaScheduleScheme;
 
 class Project extends Model
 {
     use HasFactory;
+
+    public function slaScheduleScheme(): BelongsTo
+    {
+        return $this->belongsTo(SlaScheduleScheme::class);
+    }
+
+    public function slaBreachScheme(): BelongsTo
+    {
+        return $this->belongsTo(SlaBreachScheme::class);
+    }
 }
